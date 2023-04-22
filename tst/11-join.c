564b96863d71d4acb501beb8b6f54a38d0f0cb6f
Sarah Mainguet <sarah.mainguet@bordeaux-inp.fr>
2023-04-22 20:03:35 +0200

    can't fix valgrind errors

--- tst/11-join.c
+++ tst/11-join.c
@@ -14,36 +14,33 @@
  * - thread_join() avec récupération valeur de retour, avec et sans thread_exit()
  */
 
-static void *thfunc(void *dummy __attribute__((unused)))
-{
-  thread_exit((void *)0xdeadbeef);
-  return NULL; /* unreachable, shut up the compiler */
+static void *thfunc(void *dummy __attribute__((unused))) {
+    thread_exit((void *)0xdeadbeef);
+    return NULL; /* unreachable, shut up the compiler */
 }
 
-static void *thfunc2(void *dummy __attribute__((unused)))
-{
-  return (void *)0xbeefdead;
+static void *thfunc2(void *dummy __attribute__((unused))) {
+    return (void *)0xbeefdead;
 }
 
-int main()
-{
-  thread_t th, th2;
-  int err;
-  void *res = NULL;
+int main() {
+    thread_t th, th2;
+    int err;
+    void *res = NULL;
 
-  err = thread_create(&th, thfunc, NULL);
-  assert(!err);
-  err = thread_create(&th2, thfunc2, NULL);
-  assert(!err);
+    err = thread_create(&th, thfunc, NULL);
+    assert(!err);
+    err = thread_create(&th2, thfunc2, NULL);
+    assert(!err);
 
-  err = thread_join(th, &res);
-  assert(!err);
-  assert(res == (void *)0xdeadbeef);
+    err = thread_join(th, &res);
+    assert(!err);
+    assert(res == (void *)0xdeadbeef);
 
-  err = thread_join(th2, &res);
-  assert(!err);
-  assert(res == (void *)0xbeefdead);
+    err = thread_join(th2, &res);
+    assert(!err);
+    assert(res == (void *)0xbeefdead);
 
-  printf("join OK\n");
-  return 0;
+    printf("join OK\n");
+    return 0;
 }
